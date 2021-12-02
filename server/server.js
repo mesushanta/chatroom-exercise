832ab6ec42b46542a029dd315fcf06b56ffd4e7f
Sushanta Pyakurel <sushanta@Sushantas-MacBook-Pro.local>
2021-12-02 14:47:48 +0100

    username are sent with the message and the message is stored in array

--- server/server.js
+++ server/server.js
@@ -2,20 +2,30 @@ const express = require('express');
 const http = require('http');
 app = express();
 const PORT = 9191;
+let user = false;
+let messages = [];
 
 const clientPath = `${__dirname}/../client`;
 const server = http.createServer(app);
 app.use(express.static(clientPath));
 
+
 const io = require('socket.io')(server);
+
 server.listen(PORT, () =>{
     console.log("server running on "+ PORT);
 });
 
 io.on('connection', (socket) => {
+    socket.on('setUser', (user) => {
+        socket.emit("currentUser", (user));
+    })
+
     socket.on('sendToAll', (message) =>{
-        io.emit("displayMessage", (message));
+        messages.push(message);
+        io.emit("displayMessage", (messages));
     });
+
     socket.on('sendToMe', (message) =>{
         socket.emit("displayMessage", (message));
     });

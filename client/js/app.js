16584f28f6b27e1fed0124dd511712b2855e2663
Sushanta Pyakurel <sushanta@Sushantas-MacBook-Pro.local>
2021-12-03 12:17:24 +0100

    login and register form added with basic validation

---
+++ client/js/app.js
@@ -0,0 +1,131 @@
+(function() {
+
+let socket = io.connect();
+
+// Login
+const LoginForm = document.getElementById('loginForm');
+const ShowLogin = document.getElementById('show_login_form');
+const Email = document.getElementById('email');
+const Password = document.getElementById('password');
+const LoginBtn = document.getElementById('loginBtn');
+const LoginError = document.getElementById('login_error');
+
+// Register
+const RegisterForm = document.getElementById('registerForm');
+const ShowRegister = document.getElementById('show_register_form');
+const RegisterEmail = document.getElementById('email_reg');
+const RegisterPassword = document.getElementById('password_reg');
+const RegisterUsername = document.getElementById('username_reg');
+const RegisterFullname = document.getElementById('fullname_reg');
+const RegisterBtn = document.getElementById('registerBtn');
+const RegisterError = document.getElementById('register_error');
+
+//Chat
+const SendToALlButton = document.getElementById('send_to_all');
+// const SendToMeButton = document.getElementById('send_to_me');
+const MessageInput = document.getElementById('message_input');
+const Target = document.getElementById('target');
+const Wrap = document.getElementById('wrap');
+const All_Users_list = document.getElementById('all_users_list');
+
+let new_data = [];
+let this_user = [];
+
+ShowLogin.addEventListener("click", function () {
+    RegisterForm.classList.add('hidden');
+    LoginForm.classList.remove('hidden');
+});
+
+ShowRegister.addEventListener("click", function () {
+    LoginForm.classList.add('hidden');
+    RegisterForm.classList.remove('hidden');
+});
+
+LoginBtn.addEventListener("click", function () {
+    let login_user = {
+        email: Email.value,
+        password: Password.value
+    }
+    socket.emit('login', login_user);
+});
+
+RegisterBtn.addEventListener("click", function () {
+    let register_user = {
+        username: RegisterUsername.value,
+        fullname: RegisterFullname.value,
+        email: RegisterEmail.value,
+        password: RegisterPassword.value
+    }
+    socket.emit('register', register_user);
+    console.log('one');
+});
+
+SendToALlButton.addEventListener("click", function () {
+    new_data = {
+        "user": this_user,
+        "msg": MessageInput.value
+    }
+    socket.emit('sendToAll',new_data );
+});
+
+// SendToMeButton.addEventListener("click", function () {
+//     new_data = {
+//         "user": this_user,
+//         "msg": MessageInput.value
+//     }
+//
+//     socket.emit('sendToAll',new_data );
+// });
+
+socket.on('currentUser', (user) => {
+    this_user = user;
+    document.getElementById('show_username').innerHTML = user.fullname;
+    LoginForm.classList.add('hidden');
+    Wrap.classList.remove("hidden");
+});
+
+
+socket.on('displayMessage', (message) => {
+    let data = '';
+    message.forEach(msg => {
+        data += '<div class="block w-full my-2">' +
+            '<span class="font-bold">' +
+            msg.user +
+            ' : ' +
+            '</span>' +
+            '<span>' +
+            msg.msg
+        '</span>'
+        '</div>';
+    });
+    Target.innerHTML = data;
+});
+
+socket.on('showAllUsers', (users) => {
+    let all_ol_users = "";
+    users.forEach(user => {
+        all_ol_users += '<li class="list-none px-8 my-2 flex justify-auto h-12">' +
+            ' <span class="self-center w-2 h-2 bg-green-300 rounded-full">' +
+            '</span>' +
+            '<span class="self-center mx-2 w-8 h-8 bg-blue-400 rounded-full">' +
+            '</span>' +
+            '<span class="self-center text-white rounded-full">' +
+            user.name +
+            '</span>' +
+            '</li>'
+    });
+    All_Users_list.innerHTML = all_ol_users;
+});
+
+socket.on("showLoginErrors", (errors) => {
+    LoginError.innerHTML = errors[0].error;
+});
+
+socket.on("showRegisterErrors", (errors) => {
+    if(errors[0].fullname) {
+       document.getElementById('fullname_error').innerHTML = errors[0].fullname;
+    }
+
+});
+
+})();
--- server/server.js
+++ server/server.js
@@ -1,14 +1,19 @@
 const express = require('express');
-const http = require('http');
 app = express();
+const http = require('http');
+const bcrypt = require("bcrypt");
+const salt = bcrypt.genSalt(10);
+
 const PORT = 9191;
 let messages = [];
+let all_users = []; // id, username, email, avatar, is_online, color
 let active_users = [];
 
 const clientPath = `${__dirname}/../client`;
 const server = http.createServer(app);
-app.use(express.static(clientPath));
+const Emailregex = '/(.+)@(.+){2,}\.(.+){2,}/';
 
+app.use(express.static(clientPath));
 
 const io = require('socket.io')(server);
 
@@ -17,19 +22,94 @@ server.listen(PORT, () =>{
 });
 
 io.on('connection', (socket) => {
-    socket.on('setUser', (user) => {
+
+    socket.on('login', (user) => {
+        let this_user = [];
+        let email = user.email;
+        let password = user.password;
+        let errors = [];
+
+        if(!email || email == '') {
+            this_user = all_users.filter(function(usr) {
+                return ((usr.email == email ) && (usr.password ==  bcrypt.hash(password, salt)));
+            });
+            console.log(this_user);
+
+            if(this_user.length == 1) {
+                let index = all_users.findIndex((usr => usr.email == email));
+                all_users[index].is_online = true;
+                socket.emit("displayMessage",messages);
+                socket.emit("currentUser", (this_user));
+                io.emit("showAllUsers", (active_users));
+            }
+            else {
+                errors.push({error :"Credentials does not match"});
+                socket.emit("showLoginErrors", (errors));
+            }
+        }
+
+    });
+    socket.on('register', (user) => {
+        console.log('two');
+        let this_user = [];
+        let errors = [];
         let new_user = {
             id: socket.id,
-            name: user
+            fullname: user.fullname,
+            username: user.username,
+            email: user.email,
+            password: bcrypt.hash(user.password, salt),
+            avatar: 'default.jpg',
+            is_online: true
         }
-        active_users.push(new_user);
-        socket.emit("displayMessage",messages);
-        socket.emit("currentUser", (user));
-        io.emit("showAllUsers", (active_users));
-    })
 
+
+        if(!new_user.fullname || new_user.fullname == '') {
+            errors.push({fullname : "Please enter fullname"});
+            socket.emit("showRegisterErrors", (errors));
+        }
+
+        if(!new_user.username || new_user.username == '') {
+            errors.push({username : "Please choose an username"});
+            socket.emit("showRegisterErrors", (errors));
+        }
+        else {
+            let find_user = all_users.filter(function(usr) {
+                return usr.username == new_user.username;
+            });
+            if(find_user.length > 0) {
+                errors.push({username : "Username already taken"});
+            }
+        }
+        if(!new_user.email || new_user.email == '') {
+            errors.push({email : "Please provide your email address"});
+            socket.emit("showRegisterErrors", (errors));
+        }
+        else {
+            if(!Emailregex.test(new_user.email)) {
+                errors.push({email : "Invalid email address"});
+            }
+            else {
+                let find_user = all_users.filter(function(usr) {
+                    return usr.email == new_user.email;
+                });
+                if(find_user.length > 0) {
+                    errors.push({email : "Email address is already registered"});
+                }
+            }
+        }
+
+        if(errors.length > 0) {
+            socket.emit("showRegisterErrors", (errors));
+        }
+        else {
+            all_users.push(new_user);
+            socket.emit("displayMessage",messages);
+            socket.emit("currentUser", (user.username));
+        }
+
+    });
     socket.on('sendToAll', (message) =>{
-        socket.id
         messages.push(message);
         io.emit("displayMessage", (messages));
     });
@@ -39,9 +119,13 @@ io.on('connection', (socket) => {
     });
 
     socket.on('disconnect', function() {
-        console.log(socket.id + ' has Got disconnect!');
+        active_users = active_users.filter(function(item) {
+            return item.id !== socket.id;
+        })
+        io.emit("showAllUsers", (active_users));
+        // console.log(socket.id + ' has Got disconnect!');
     });
-});
 
+});
 
 
